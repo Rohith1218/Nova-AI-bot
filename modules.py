27e2e4bfc2cb2ce43a4c9af72b5329e75eb87f06
Rohith Reddy <94784696+Rohith1218@users.noreply.github.com>
2023-07-07 22:04:40 +0530

    Create modules.py

---
+++ modules.py
@@ -0,0 +1,16 @@
+import requests
+import speech_recognition as sr
+import os
+import webbrowser
+from gtts import gTTS
+import datetime
+import pywhatkit
+import cv2
+import nltk
+import string
+import random
+import json
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from tensorflow import keras
